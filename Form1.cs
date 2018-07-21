4d877e8bfe493bfabd75bdf9c83d0bba98da30ba
Rutile_Darkness3_for_DESKTOP-8MKUBO2 <Rutile3@outlook.jp>
2018-07-21 16:17:25 +0900

    大まかな処理を作成

--- Form1.cs
+++ Form1.cs
@@ -14,20 +14,55 @@ namespace Incrementer {
             InitializeComponent();
         }
         private void MainForm_Load(object sender, EventArgs e) {
-            
+            StringBuilder tmp = new StringBuilder();
+            tmp.AppendLine("row(");
+            tmp.AppendLine("row(123");
+            tmp.AppendLine("rows(");
+            tmp.AppendLine("rows(123");
+            tmp.AppendLine("row()");
+            tmp.AppendLine("row(123)");
+            tmp.AppendLine("rows()");
+            tmp.AppendLine("rows(123)");
+            tmp.AppendLine("row(");
+            targetTextBox.Text = tmp.ToString();
+            frontTextBox.Text = "row(";
         }
 
         private void IncrementButton_Click(object sender, EventArgs e) {
             if (targetTextBox.Text == "" || frontTextBox.Text == "")
                 return;//未入力時は即リターン
 
+            string front_text = frontTextBox.Text;
             string[] split_string = { "\r\n" };
             string[] rows = targetTextBox.Text.Split(split_string, StringSplitOptions.None);
             StringBuilder result = new StringBuilder();
 
             foreach(string row in rows) {
+                int index = row.IndexOf(front_text);
+                if (index != -1) {
+                    index += front_text.Length;
+                    result.Append(row.Substring(0, index));
 
+                    bool exists_target_value = false;
+                    decimal target_value = 0;
+                    while (index < row.Length && '0' <= row[index] && row[index] <= '9') {
+                        exists_target_value = true;
+                        target_value *= 10;
+                        target_value += decimal.Parse(new string(new char[] { row[index]}));
+                        index++;
+                    }
+                    if (exists_target_value) {
+                        target_value += incrementTextBox.Value;
+                        result.Append(target_value.ToString());
+                    }
+                    result.AppendLine(row.Substring(index));
+                }
+                else
+                    result.AppendLine(row);
             }
+
+            //ややこしいが、最終行で改行しないようにしてます
+            targetTextBox.Text = result.Remove(result.Length - 2, 2).ToString();
         }
     }
 }
